cfcc81bcdba5c459caebb315977863d62e056b07
akihir016 <achilles.bitangcol@gmail.com>
2024-09-20 13:27:07 +0800

    error

--- src/components/ButtonAppbar.jsx
+++ src/components/ButtonAppbar.jsx
@@ -50,7 +50,7 @@ function DrawerAppBar(props) {
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
       <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
+        CHAAATUP
       </Typography>
       <Divider />
       <List>
